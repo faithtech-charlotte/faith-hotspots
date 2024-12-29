4374fa4f56a634f2d00c7551663554923dfff5f1
Brad Linard <brad.linard@idelata.com>
2024-12-29 18:57:41 -0500

    Adding NestJS with WebSockets api

---
+++ apps/server/src/websockets/websockets.gateway.ts
@@ -0,0 +1,25 @@
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway(5010)
+export class WebsocketsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  private logger = new Logger('WebsocketsGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('WebSocket server initialized');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log('Client connected');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log('Client disconnected');
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: any): string {
+    return 'Hello world!';
+  }
+}
